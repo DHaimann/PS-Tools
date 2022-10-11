a14b011a7d8edff68fa8ae265b6ae01bc1ec5942
Dietmar Haimann <58327672+DHaimann@users.noreply.github.com>
2022-10-11 17:58:15 +0200

    Add files via upload

---
+++ Get-asPublisherRules.ps1
@@ -0,0 +1,100 @@
+﻿[CmdletBinding()]
+
+Param (
+    [ValidateScript({Test-Path -Path $_ -PathType 'Leaf'})]
+    [Parameter(Mandatory=$true)]
+    $EvtxImportFile
+)
+
+Function Set-XMLElement {
+    # Create new File Publisher Rule
+    $NewFilePublisherRule = $RuleCollection.AppLockerPolicy.RuleCollection.AppendChild($RuleCollection.CreateElement('FilePublisherRule'))
+    $NewFilePublisherRule.SetAttribute('Id', $FilePublisherRule.Id)
+    $NewFilePublisherRule.SetAttribute('Name', "Publisher: $($FilePublisherCondition.PublisherName)")
+    $NewFilePublisherRule.SetAttribute('Description', "Product: $($FilePublisherCondition.ProductName) - Found in: $PublisherPath")
+    $NewFilePublisherRule.SetAttribute('UserOrGroupSid', 'S-1-1-0')
+    $NewFilePublisherRule.SetAttribute('Action', 'Allow')
+    
+    # Create new Conditions
+    $NewConditions = $NewFilePublisherRule.AppendChild($RuleCollection.CreateElement('Conditions'))
+       
+    # Create new Publisher Condition
+    $NewFilePublisherCondition = $NewConditions.AppendChild($RuleCollection.CreateElement('FilePublisherCondition'))
+    $NewFilePublisherCondition.SetAttribute('PublisherName', $($FilePublisherCondition.PublisherName))
+    $NewFilePublisherCondition.SetAttribute('ProductName', '*')
+    $NewFilePublisherCondition.SetAttribute('BinaryName', '*')
+    
+    # Create new BinaryVersionRange
+    $NewBinaryVersionRange = $NewFilePublisherCondition.AppendChild($RuleCollection.CreateElement('BinaryVersionRange'))
+    $NewBinaryVersionRange.SetAttribute('LowSection', '*')
+    $NewBinaryVersionRange.SetAttribute('HighSection', '*')
+} # Function
+
+
+# Create PublisherRules.xml
+$XMLPath = "C:\Util\AppLocker\PublisherRules.xml"
+
+If (!(Test-Path -Path $XMLPath)) {
+    New-Item -Path $XMLPath -ItemType File -Force
+
+    $StandardXMLFile = [xml]@"
+        <AppLockerPolicy Version="1">
+            <RuleCollection Type="Exe" EnforcementMode="NotConfigured">
+            </RuleCollection>
+        </AppLockerPolicy>
+"@
+
+    $StandardXMLFile.Save($XMLPath)
+}
+
+# Read content of PublisherRules.xml
+[xml]$RuleCollection = Get-Content -Path $XMLPath
+
+# Gather AppLocker file information
+$Publishers = Get-AppLockerFileInformation -EventLog -LogPath $EvtxImportFile | Where-Object -FilterScript { $_.Publisher -ne $null }
+
+foreach ($Publisher in $Publishers) {
+    
+    # Set Variables from Publisher
+    $PublisherPublisherName = $Publisher.Publisher.PublisherName
+    $PublisherPublisherProductName = $Publisher.Publisher.ProductName
+    $PublisherPublisherBinaryName = $Publisher.Publisher.BinaryName
+    $PublisherPublisherBinaryVersion = $Publisher.Publisher.BinaryVersion
+    
+    [string]$PublisherPath = $Publisher.Path
+    
+    # Workaround for missing product name and binary for lazy developers
+    If ($PublisherPublisherProductName -eq $null -or $PublisherPublisherProductName -eq '') {$Publisher.Publisher.ProductName = $(($PublisherPath.Split('\\')[-1]).Split('.')[0])}
+    If ($PublisherPublisherBinaryName -eq $null -or $PublisherPublisherBinaryName -eq '') {$Publisher.Publisher.BinaryName = $($PublisherPath.Split('\\')[-1])}
+
+    # Generate AppLocker policy
+    Try {
+        [xml]$Policy = New-AppLockerPolicy -FileInformation $Publisher -RuleType Publisher -RuleNamePrefix Publisher -User Jeder -Xml -Optimize -ErrorAction Stop
+    }
+    Catch {
+        Write-Host "Could not create AppLocker Policy for $PublisherPublisherName!" -ForegroundColor Red
+        Continue
+    }    
+
+    # Set variables
+    $RuleNamePrefix = "Publisher: "
+    $FilePublisherRule = $Policy.AppLockerPolicy.RuleCollection.FilePublisherRule
+    $FilePublisherCondition = $FilePublisherRule.Conditions.FilePublisherCondition
+    $RuleCollectionPublisherNames = $RuleCollection.AppLockerPolicy.RuleCollection.FilePublisherRule.Conditions.FilePublisherCondition.PublisherName
+    
+    # Check if PublisherRules.xml is empty
+    If (!($RuleCollectionPublisherNames -eq $null)) {
+        # Check if rule already exists
+        If ($RuleCollectionPublisherNames.Contains($PublisherPublisherName)) {
+            Write-Host "Already exists: $PublisherPublisherName" -ForegroundColor Yellow
+            Continue
+        } Else {
+            Set-XMLElement
+        }
+    } Else {
+        Set-XMLElement
+    }
+}
+
+# Save PublisherRules.xml
+$RuleCollection.Save($XMLPath)
