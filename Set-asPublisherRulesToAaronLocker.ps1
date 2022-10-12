e266e43c866aa6a0355f74892f0fa9f7f24e8573
Dietmar Haimann <58327672+DHaimann@users.noreply.github.com>
2022-10-12 10:20:08 +0200

    Update Set-asPublisherRulesToAaronLocker.ps1
    removed unnecessary code

--- Set-asPublisherRulesToAaronLocker.ps1
+++ Set-asPublisherRulesToAaronLocker.ps1
@@ -14,7 +14,7 @@ Function Set-XMLElement {
     $Element = @"
             
 @{
-  label = "Publisher: $PublisherPublisherName - File: $PublisherPublisherBinaryName";
+  label = "File: $PublisherPublisherBinaryName";
   PublisherName = "$PublisherPublisherName";
   RuleCollection = "Exe";
 }
@@ -30,15 +30,10 @@ foreach ($Publisher in $Publishers) {
     
     # Set Variables from Publisher
     $PublisherPublisherName = $Publisher.Publisher.PublisherName
-    $PublisherPublisherProductName = $Publisher.Publisher.ProductName
     $PublisherPublisherBinaryName = $Publisher.Publisher.BinaryName
     
     [string]$PublisherPath = $Publisher.Path
     
-    # Workaround for missing product name and binary for lazy developers
-    If ($PublisherPublisherProductName -eq $null -or $PublisherPublisherProductName -eq '') {$Publisher.Publisher.ProductName = $(($PublisherPath.Split('\\')[-1]).Split('.')[0])}
-    If ($PublisherPublisherBinaryName -eq $null -or $PublisherPublisherBinaryName -eq '') {$Publisher.Publisher.BinaryName = $($PublisherPath.Split('\\')[-1])}
-
     # Check if rule already exists in TrustedPublishers by publisher name
     $ExistingRule = Select-String -Path $TrustedSignersFile -Pattern $PublisherPublisherName -SimpleMatch -Quiet
 
