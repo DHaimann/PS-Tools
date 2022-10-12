ad0c98fa12e5346cf24830839159d4c76d7f52e5
Dietmar Haimann <58327672+DHaimann@users.noreply.github.com>
2022-10-12 15:29:40 +0200

    Update Set-asPublisherRulesToAaronLocker.ps1
    Added a way to import an csv file to exclude untrusted publishers.

--- Set-asPublisherRulesToAaronLocker.ps1
+++ Set-asPublisherRulesToAaronLocker.ps1
@@ -1,4 +1,4 @@
-﻿[CmdletBinding()]
+[CmdletBinding()]
 
 Param (
     [ValidateScript({Test-Path -Path $_ -PathType 'Leaf'})]
@@ -7,14 +7,18 @@ Param (
 
     [ValidateScript({Test-Path -Path $_ -PathType 'Leaf'})]
     [Parameter(Mandatory=$true)]
-    $TrustedSignersFile
+    $TrustedSignersFile,
+
+    [ValidateScript({Test-Path -Path $_ -PathType 'Leaf'})]
+    [Parameter(Mandatory=$false)]
+    $UnTrustedPublishersFile
 )
 
 Function Set-XMLElement {
     $Element = @"
             
 @{
-  label = "File: $PublisherPublisherBinaryName";
+  label = "Publisher $PublisherPublisherProductName $PublisherPublisherBinaryName";
   PublisherName = "$PublisherPublisherName";
   RuleCollection = "Exe";
 }
@@ -23,6 +27,9 @@ Function Set-XMLElement {
     Add-Content -Path $TrustedSignersFile -Value $Element -Force
 } # Function
 
+# Import UnTrusted Signers File
+$UnTrustedPublishers = (Import-Csv -Path $UnTrustedSignersFile -Delimiter ';').UnTrustedPublisher
+
 # Gather AppLocker file information
 $Publishers = Get-AppLockerFileInformation -EventLog -LogPath $EvtxImportFile | Where-Object -FilterScript { $_.Publisher -ne $null }
 
@@ -30,7 +37,9 @@ foreach ($Publisher in $Publishers) {
     
     # Set Variables from Publisher
     $PublisherPublisherName = $Publisher.Publisher.PublisherName
+    $PublisherPublisherProductName = $Publisher.Publisher.ProductName
     $PublisherPublisherBinaryName = $Publisher.Publisher.BinaryName
+    $PublisherPublisherBinaryVersion = $Publisher.Publisher.BinaryVersion
     
     [string]$PublisherPath = $Publisher.Path
     
@@ -43,13 +52,19 @@ foreach ($Publisher in $Publishers) {
             $PublisherPublisherBinaryName = "Missing in Cert"
         }
     }
+
+        
+    [string]$PublisherPath = $Publisher.Path
     
     # Check if rule already exists in TrustedPublishers by publisher name
     $ExistingRule = Select-String -Path $TrustedSignersFile -Pattern $PublisherPublisherName -SimpleMatch -Quiet
 
     If ($ExistingRule) {
         Continue
+    } ElseIf ($UnTrustedPublishers.Contains($PublisherPublisherName)) {
+        Continue
     } Else {
         Set-XMLElement
-    }
+    }        
+
 }
